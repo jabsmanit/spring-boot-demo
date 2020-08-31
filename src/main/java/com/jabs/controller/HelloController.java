4cf653c8abda20c9254a728113b89aa16e9778ff
Jabir Hussain <jabir.hussain@ericsson.com>
2020-08-31 19:54:02 +0530

    Initial draft

---
+++ src/main/java/com/jabs/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.jabs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/test")
+	public String test() {
+		return "Hello from Spring Boot";
+	}
+}
